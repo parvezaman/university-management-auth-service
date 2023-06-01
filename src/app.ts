b2138f8660bffe9667267d416f7ea554078f11f5
parvezaman <emailtoamanullah@gmail.com>
2023-06-01 06:27:21 +0600

    [done] user services and creating a new user via post request

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.routes'
 
 const app: Application = express()
 
@@ -9,7 +10,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application routes
+app.use('/api/v1/users', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('Hello World! I have started the server')
 })
 
